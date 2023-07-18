8b002bd022e01b39923f70278411168176a271c3
Steven Kuhfahl <31016858+skuhfahl@users.noreply.github.com>
2023-07-18 14:24:19 -0500

    Refactor file names & plugin properties

--- src/main/java/com/BigBananaNotifier/BigBananaNotifierPlugin.java
+++ src/main/java/com/BigBananaNotifier/BigBananaNotifierPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.BigBananaNotifier;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Big Banana Notifier"
 )
-public class ExamplePlugin extends Plugin
+public class BigBananaNotifierPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private BigBananaNotifierConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	BigBananaNotifierConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(BigBananaNotifierConfig.class);
 	}
 }
--- src/test/java/com/BigBananaNotifier/BigBananaNotifierPluginTest.java
+++ src/test/java/com/BigBananaNotifier/BigBananaNotifierPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.BigBananaNotifier;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class BigBananaNotifierPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(BigBananaNotifierPlugin.class);
 		RuneLite.main(args);
 	}
 }
